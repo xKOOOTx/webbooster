04ffc2f83299bbae17f87079462812a5f663dd32
KOOOT <kooot123@gmail.com>
2021-07-12 02:52:59 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,47 @@
+const productSection = document.querySelectorAll('.product');
+const closeForm = document.querySelector('.fa-times');
+const form = document.querySelector('.form');
+const formProductName = document.querySelector('.form__productName')
+
+
+productSection.forEach(el => {
+    const productButton = el.querySelector('.product__button');
+    const productName = el.querySelector('.product__name');
+
+
+    productButton.addEventListener('click', () => {
+        form.classList.add('form-active');
+        formProductName.textContent = '';
+        formProductName.innerHTML = productName.innerHTML;
+    })
+})
+
+//  closing form by clicking outside
+form.addEventListener('click', el => {
+    if (el.target.classList.contains('form-active')) {
+        form.classList.remove('form-active')
+    }
+})
+
+//  close from button
+closeForm.addEventListener('click', () => {
+    form.classList.remove('form-active')
+})
+
+//  close form button animation
+closeForm.addEventListener('mouseout', () => {
+    closeForm.animate([
+        {transform: 'rotate(-90deg)'}
+    ], {
+        duration: 200,
+        iterations: 2
+    })
+})
+closeForm.addEventListener('mouseenter', () => {
+    closeForm.animate([
+        {transform: 'rotate(90deg)'}
+    ], {
+        duration: 200,
+        iterations: 2
+    })
+})
