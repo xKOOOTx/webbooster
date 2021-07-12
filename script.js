39287061193ba8ed8e21a137741ec642eda5f3fa
xKOOOTx <kooot123@gmail.com>
2021-07-12 16:28:08 +0300

    making post request

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const productSection = document.querySelectorAll('.product');
 const closeForm = document.querySelector('.fa-times');
-const form = document.querySelector('.form');
+const form = document.getElementById('form');
+const formWrapper = document.querySelector('.form')
 const formProductName = document.querySelector('.form__productName')
 
 
@@ -10,19 +11,19 @@ productSection.forEach(el => {
 
 
     productButton.addEventListener('click', () => {
-        form.classList.add('form-active');
+        formWrapper.classList.add('form-active');
         formProductName.textContent = '';
         formProductName.innerHTML = productName.innerHTML;
     })
 })
 
 //  closing form by clicking outside
-form.addEventListener('click', el => {
+formWrapper.addEventListener('click', el => {
     if (el.target.classList.contains('form-active')) {
-        form.classList.remove('form-active')
+        formWrapper.classList.remove('form-active')
     }
     if (el.target.classList.contains('form__closeBtn')) {
-        form.classList.remove('form-active')
+        formWrapper.classList.remove('form-active')
     }
 })
 
@@ -43,3 +44,75 @@ closeForm.addEventListener('mouseenter', () => {
         iterations: 2
     })
 })
+
+document.addEventListener('DOMContentLoaded', () => {
+    form.addEventListener('submit', formSend)
+
+    async function formSend(e) {
+        e.preventDefault();
+
+        let error = formValidate(form);
+
+        let formData = new FormData(form);
+
+        if (!error === 0) {
+            alert('Заполните все поля')
+        } else {
+            console.log('sending')
+            form.classList.add('_sending');
+            let response = await fetch('sendmail.php', {
+                method: 'POST',
+                body: formData
+            })
+
+            if (response.ok) {
+                let result = await response.json();
+                alert(result.message)
+                form.reset();
+            } else {
+                alert('Ошибка')
+            }
+        }
+    }
+
+    function formValidate(form) {
+        let error = 0;
+        let formReq = document.querySelectorAll('._req');
+
+        for (let i = 0; i < formReq.length; i++) {
+            const input = formReq[i];
+            formRemoveError(input);
+
+            if (input.classList.contains('_email')) {
+                if(emailTest(input)) {
+                    formAddError(input);
+                    error++;
+                }
+            } else if (input.getAttribute('type') === 'checkbox' && input.checked === false) {
+                formAddError(input);
+                error++;
+            } else {
+                if (input.value === '') {
+                    formAddError(input);
+                    error++;
+                }
+            }
+        }
+    }
+
+    function formAddError(input) {
+        input.parentElement.classList.add('_error');
+        input.classList.add('_error');
+    }
+    function formRemoveError(input) {
+        input.parentElement.classList.remove('_error');
+        input.classList.remove('_error');
+    }
+    function emailTest(input) {
+        return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+    }
+
+
+})
+
+// return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
